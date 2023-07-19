753a71015acf75c593e411332b84ed3bdd2e3009
weishaodaren <zhaojiawei@startable.cn>
2023-07-19 15:14:11 +0800

    feat: 光标 位置 角度 跟随

---
+++ index.ts
@@ -0,0 +1,12 @@
+const cursor = document.querySelector('.cursor') as HTMLElement;
+
+
+window.onmousemove = (event: MouseEvent) => {
+  const { movementX, movementY, clientX, clientY } = event;
+
+  const rotate = Math.abs(movementX) + Math.abs(movementY) > 5 // 光标滑动阈值
+    ? Math.atan2(movementX, -movementY) // Y轴相反
+    : 0;
+  
+  cursor.style.transform = `translate3d(${clientX}px, ${clientY}px, 0) rotate(${rotate}rad)`;
+}
